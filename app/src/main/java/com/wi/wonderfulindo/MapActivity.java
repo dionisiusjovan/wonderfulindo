9787a11ab41ceeaad76a98f72a213790c1ce66c9
Dionisius Jovan <672017240@student.uksw.edu>
2020-04-09 14:36:45 +0700

    --Location Permission

--- app/src/main/java/com/wi/wonderfulindo/MapActivity.java
+++ app/src/main/java/com/wi/wonderfulindo/MapActivity.java
@@ -1,9 +1,16 @@
 package com.wi.wonderfulindo;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
@@ -18,6 +25,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     GoogleMap map;
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+    private static final String TAG = "MapActivity";
     double lat = -7.607735;
     double lng = 110.204441;
     String name = "Borobudur";
@@ -38,6 +47,22 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("This app needs location access");
+                builder.setMessage("Please Grant location access so this app can work properly");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+                    }
+                });
+                builder.show();
+            }
+        }
     }
 
     @Override
@@ -48,7 +73,29 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         map.addMarker(new MarkerOptions().position(inputLatLong).title(name));
         //map.moveCamera(CameraUpdateFactory.newLatLng(inputLatLong));
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(inputLatLong, 15.0f));
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Log.d(TAG, "onRequestPermissionsResult: coarse location permission granted.");
+                } else{
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionally Limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover places when in the background.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
 
+                        }
+                    });
+                    builder.show();
+                }
+                return;
+            }
+        }
     }
 }
